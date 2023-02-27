bf1bb8791da8d64086d1bc028302566856aec638
catarinaluz <catarinaluz@usp.br>
2023-02-27 19:40:14 -0300

    Atualizando projeto1

--- projeto1/catkin_ws/src/projeto1/scripts/cmd-vel.py
+++ projeto1/catkin_ws/src/projeto1/scripts/cmd-vel.py
@@ -8,6 +8,11 @@ def move():
     vel_msg = Twist()
 
     speed = input("Speed: ")
+    tempo_a = float(input("Tempo andando: "))
+    tempo_p = float(input("Tempo parado: "))
+
+    num_loops = int(input("Apos quantos loops mudar os parametros: "))
+
 
     vel_msg.linear.x = float(speed)
 
@@ -17,13 +22,14 @@ def move():
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
 
+    loop = 0
 
 
     while not rospy.is_shutdown():
 
         t0 = rospy.get_time()
         current_time = 0
-        while(current_time<3):
+        while(current_time<tempo_a):
             vel_msg.linear.x = float(speed)
             velocity_publisher.publish(vel_msg)
             t1 = rospy.get_time()
@@ -34,7 +40,7 @@ def move():
         
         current_time = 0
         t2 = rospy.get_time()
-        while(current_time<3):
+        while(current_time<tempo_p):
             vel_msg.linear.x=0
             velocity_publisher.publish(vel_msg)
             t3 = rospy.get_time()
@@ -43,6 +49,16 @@ def move():
             print('PARADO')
             print('[TIME-P: ]',current_time)
             print('[SPEED-P: ]',vel_msg.linear.x)
+        
+        loop+=1
+
+        if loop == num_loops:
+            print('[INFO] Mudar parametros')
+            speed = input("Speed: ")
+            tempo_a = float(input("Tempo andando: "))
+            tempo_p = float(input("Tempo parado: "))
+
+            loop = 0
 
 if __name__ == '__main__':
     try:
